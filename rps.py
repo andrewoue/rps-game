5040401b4e8996f549a6579c490787ee99a6e028
Andrew Ouellette <ouellette.andrew@gmail.com>
2019-03-20 23:10:33 -0500

    Updated comments and cleaned up code

--- rps.py
+++ rps.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#RPS Udacity Project 1.0, Andrew Ouellette
+
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
@@ -12,10 +14,11 @@ import random
 
 class Player:
     def move(self):
-        return random.choice(moves)
+        pass
 
     def learn(self, my_move, their_move):
         pass
+#Parent Player Class
 
 class HumanPlayer(Player):
     def move(self):
@@ -24,18 +27,22 @@ class HumanPlayer(Player):
             print("I don't understand, please try again.")
             decision = input('rock, paper, or scissors... ')
         return decision
+#Human Player class
 
 class RockPlayer(Player):
     def move(self):
         return moves[0]
+#This Player class always selects rock
 
 class RandomPlayer(Player):
     def move(self):
         return random.choice(moves)
+#This Player class selects a random move
 
 class CyclePlayer(Player):
     def move(self):
         return moves[Game.round_count]
+#This Player class follows the order - rock, paper, scissors
 
 class CopyPlayer(Player):
     def move(self):
@@ -49,6 +56,7 @@ class CopyPlayer(Player):
     def learn(self, my_move, their_move):
         my_move = Game.p2_move_list
         thier_move = Game.p1_move_list
+#This Player class copies the Human Player class selections
 
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
@@ -79,6 +87,7 @@ class Game:
             print("I don't understand, please try again.")
             games = input("1 or 3? ")
         Game.game_count = int(games)
+    #Allows player to select between a single round or three rounds
 
     def play_round(self):
         move1 = self.p1.move()
@@ -86,8 +95,6 @@ class Game:
         move2 = self.p2.move()
         Game.p2_move_list.append(move2)
         print(f"Player 1: {move1}  Player 2: {move2}")
-        #self.p1.learn(move1, move2)
-        #self.p2.learn(move2, move1)
         if beats(move1, move2):
             print("*** PLAYER ONE WINS ***")
             self.p1.score += 1
@@ -113,8 +120,7 @@ class Game:
             print("*** THIS GAME IS A TIE ***")
         print(f"*** FINAL SCORE: {self.p1.score} TO {self.p2.score} ***")
 
-
 if __name__ == '__main__':
-    game = Game(Player(), select_opponent())
+    game = Game(HumanPlayer(), select_opponent())
     game.game_select()
     game.play_game()
